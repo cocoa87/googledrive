3361325638469a38b2ff73eac6526c47f94fdd0f
cocoa87 <121846220+cocoa87@users.noreply.github.com>
2023-02-21 13:11:50 +0900

    commit

---
+++ notification/src/main/java/untitled/infra/PolicyHandler.java
@@ -0,0 +1,53 @@
+package untitled.infra;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.naming.NameParser;
+import javax.naming.NameParser;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+import untitled.config.kafka.KafkaProcessor;
+import untitled.domain.*;
+
+@Service
+@Transactional
+public class PolicyHandler {
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whatever(@Payload String eventString) {}
+
+    @StreamListener(
+        value = KafkaProcessor.INPUT,
+        condition = "headers['type']=='VideoStreamingProcessed'"
+    )
+    public void wheneverVideoStreamingProcessed_NotifyToUser(
+        @Payload VideoStreamingProcessed videoStreamingProcessed
+    ) {
+        VideoStreamingProcessed event = videoStreamingProcessed;
+        System.out.println(
+            "\n\n##### listener NotifyToUser : " +
+            videoStreamingProcessed +
+            "\n\n"
+        );
+        // Sample Logic //
+
+    }
+
+    @StreamListener(
+        value = KafkaProcessor.INPUT,
+        condition = "headers['type']=='FileUploaded'"
+    )
+    public void wheneverFileUploaded_NotifyToUser(
+        @Payload FileUploaded fileUploaded
+    ) {
+        FileUploaded event = fileUploaded;
+        System.out.println(
+            "\n\n##### listener NotifyToUser : " + fileUploaded + "\n\n"
+        );
+        // Sample Logic //
+
+    }
+}
